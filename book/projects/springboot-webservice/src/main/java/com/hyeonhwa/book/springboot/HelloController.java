7d92f9edfaa64e71fd3dea1a0781a6a2c4b54f8f
crunky78 <sainthxx@kakao.com>
2021-04-28 18:04:05 +0900

    test

---
+++ book/projects/springboot-webservice/src/main/java/com/hyeonhwa/book/springboot/HelloController.java
@@ -0,0 +1,13 @@
+package com.hyeonhwa.book.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
