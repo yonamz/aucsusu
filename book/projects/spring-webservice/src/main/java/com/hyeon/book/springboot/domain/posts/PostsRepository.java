571e05a405f9eaeaaffc50352e623966c7c3606d
crunky78 <sainthxx@kakao.com>
2021-05-12 17:09:45 +0900

    프로젝트 생성

--- book/projects/spring-webservice/src/main/java/com/hyeon/book/springboot/domain/posts/PostsRepository.java
+++ book/projects/spring-webservice/src/main/java/com/hyeon/book/springboot/domain/posts/PostsRepository.java
@@ -1,6 +1,12 @@
 package com.hyeon.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts,Long> {            //jparepository<entity타입, pk타입>
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
