80c0d4a59e1b6713222afb1bb9e84b99362ea6f0
crunky78 <sainthxx@kakao.com>
2021-06-25 22:38:11 +0900

    commit

--- oksusu/src/main/java/com/yonamz/user/domain/User.java
+++ oksusu/src/main/java/com/yonamz/user/domain/User.java
@@ -1,4 +1,24 @@
 package com.yonamz.user.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String uid;
+
+    @Column
+    private String name;
+
+
 }
