f485522bc9a413bfadff499641e87558a0e0c21d
crunky78 <73973086+crunky78@users.noreply.github.com>
2021-07-27 15:32:59 +0900

    Delete aucsusu-master directory

--- aucsusu-master/src/main/java/com/yonamz/aucsusu/item/ItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.yonamz.aucsusu.item;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemRepository extends JpaRepository<Item, Integer> {
-}
