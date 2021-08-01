454d3ba0824ab815e2365bd8a0285631e0d03657
crunky78 <73973086+crunky78@users.noreply.github.com>
2021-08-01 21:19:28 +0900

    Delete IdeaProjects/blog/blogProject directory

--- IdeaProjects/blog/blogProject/src/main/java/com/hyeonhwa/blog/springboot/domain/comment/CommentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.hyeonhwa.blog.springboot.domain.comment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
